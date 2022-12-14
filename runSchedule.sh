7a29974557fddc204f027a00f4036786304f12fc
Mario-69-Mario <84464653+Mario-69-Mario@users.noreply.github.com>
2022-12-14 13:43:25 -0800

    Update and rename runSchedule to runSchedule.sh

--- runSchedule.sh
+++ runSchedule.sh
@@ -13,6 +13,12 @@
 
 # chmod 755 runSchedule
 # export NOTION_TOKEN=
+#0-59 * * * * /Users/jaai/anaconda3/bin/python3/python ~/PycharmProjects/dailySearch.py trees >> ~/woah.log 2>&1
 
-python3 notion-scheduleler.py >> log.txt
-tail log.txt
+token.sh
+git# git clone https://mario-69-mario@github.com/Mario-69-Mario/notion-scheduleler /
+cd /git
+git pull
+cd /app
+cp *.py . 
+python3 /app/notion-scheduleler.py
