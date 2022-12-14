2b6c4168485c9ed63cc8cefcd691bedbcf8b467c
Mario-69-Mario <84464653+Mario-69-Mario@users.noreply.github.com>
2022-12-14 13:58:08 -0800

    Update buildContainer.sh

--- buildContainer.sh
+++ buildContainer.sh
@@ -1,5 +1,9 @@
-echo "# Auto build from buildContainer script"  > token.sh
-echo export NOTION_TOKEN=$NOTION_TOKEN >> token.sh
-echo export PYTHONPATH=/app/appRequirements >> token.sh
+echo "# Auto build from buildContainer script"  > crontab
+echo export NOTION_TOKEN=$NOTION_TOKEN >> crontab
+echo export PYTHONPATH=/app/appRequirements >> crontab
+echo " " >> crontab
+echo "# Run automation every min" >> crontab
+echo "* * * * * bash /app/runSchedule.sh >> /var/log/myjob.log 2>&1" >> crontab
+
 
 docker build -t my-python --build-arg NOTION_TOKEN=$NOTION_TOKEN .
