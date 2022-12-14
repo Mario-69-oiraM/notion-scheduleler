d97001c7e48671399424e14d21edc2ee5680804c
Mario-69-Mario <84464653+Mario-69-Mario@users.noreply.github.com>
2022-12-14 13:46:43 -0800

    Update buildContainer.sh

--- buildContainer.sh
+++ buildContainer.sh
@@ -1,3 +1,5 @@
 echo "# Auto build from buildContainer script"  > token.sh
 echo export NOTION_TOKEN=$NOTION_TOKEN >> token.sh
 echo export PYTHONPATH=/app/appRequirements >> token.sh
+
+docker build -t my-python --build-arg NOTION_TOKEN=$NOTION_TOKEN .
