4413ab1147b2a544f35ec6906803fa085ffd68a3
Mario-69-Mario <84464653+Mario-69-Mario@users.noreply.github.com>
2022-12-14 14:17:08 -0800

    Update runSchedule.sh

--- runSchedule.sh
+++ runSchedule.sh
@@ -15,10 +15,12 @@
 # export NOTION_TOKEN=
 #0-59 * * * * /Users/jaai/anaconda3/bin/python3/python ~/PycharmProjects/dailySearch.py trees >> ~/woah.log 2>&1
 
-token.sh
-git# git clone https://mario-69-mario@github.com/Mario-69-Mario/notion-scheduleler /
-cd /git
+cd /
+mkdir temp-git
+cd temp-git
+git# git clone https://mario-69-mario@github.com/Mario-69-Mario/notion-scheduleler .
+cd /temp-git
 git pull
 cd /app
-cp /git/*.py . 
+cp /temp-git/*.py . 
 python3 /app/notion-scheduleler.py
